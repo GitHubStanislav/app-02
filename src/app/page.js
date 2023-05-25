945fe536fb8dd3f7ce1b0a5fd0af4fbfb52bf1ff
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-05-25 16:58:15 +0300

    +

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,5 @@
 "use client";
-import { useState } from "react";
-import CounterFirst from "@/app/components/CounterFirst";
-import MyInputFirst from "@/app/components/MyInputFirst";
 
 export default function Home() {
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState("");
-  const increment = () => {
-    setLikes(likes + 1);
-  };
-  const decrement = () => {
-    setLikes(likes - 1);
-  };
-  const handleInputChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  return (
-    <div className="flex flex-col items-start m-9 ">
-      <MyInputFirst value={value} onChangeInput={handleInputChange} />
-      <CounterFirst
-        likes={likes}
-        incrementHandler={increment}
-        decrementHandler={decrement}
-      />
-    </div>
-  );
+  return <></>;
 }
