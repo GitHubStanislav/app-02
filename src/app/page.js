929193993faf041898f005458fc6076d7cc55437
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-05-27 11:47:10 +0300

    empty list message

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,38 @@
 "use client";
-
+import { useState } from "react";
 import ListBio from "@/app/formTest/ListBio";
 import FormTestInput from "@/app/formTest/FormTestInput";
 import DATA_BIO from "@/app/formTest/data";
-import { useState } from "react";
-export default function Home() {
+
+function Home() {
+  const emptyListMessage = (
+    <h2 className="text-xl text-center">Your list is empty</h2>
+  );
   const [dataListElements, setDataListElements] = useState(DATA_BIO);
+
   const addUsersHandler = (user) => {
     setDataListElements((prevUsers) => [...prevUsers, user]);
   };
+
   const removeListElement = (user) => {
     setDataListElements((prevUsers) =>
       prevUsers.filter((u) => u.id !== user.id)
     );
   };
+
   return (
     <>
       <FormTestInput addUsers={addUsersHandler} />
-      <ListBio
-        removeHandler={removeListElement}
-        dataListElements={dataListElements}
-      />
+      {dataListElements.length !== 0 ? (
+        <ListBio
+          removeHandler={removeListElement}
+          dataListElements={dataListElements}
+        />
+      ) : (
+        emptyListMessage
+      )}
     </>
   );
 }
+
+export default Home;
