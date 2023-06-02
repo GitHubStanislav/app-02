3017324ae27149fa411af941dcf9965877d9adbb
Stanislav <spiridonov.kyiv@gmail.com>
2023-06-02 20:49:05 +0300

    +

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import MyHeader from "@/app/cryptoCrypto/MyHeader";
+import App1 from "@/app/CoinApi/App1";
 
 function Home() {
   return (
     <>
       <MyHeader />
+      <App1 />
     </>
   );
 }
