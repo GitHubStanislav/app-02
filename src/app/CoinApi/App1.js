d61cf2059f8f3591e9425950783de2dbb0416cec
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-06-03 17:30:51 +0300

    +

--- src/app/CoinApi/App1.js
+++ src/app/CoinApi/App1.js
@@ -46,24 +46,84 @@ const App1 = () => {
     setCurrentPage(pageNumber);
   };
 
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    const totalPages = Math.ceil(cryptocurrencies.length / 10);
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   const renderPagination = () => {
     const totalPages = Math.ceil(cryptocurrencies.length / 10);
     const paginationItems = [];
 
-    for (let i = 1; i <= totalPages; i++) {
-      paginationItems.push(
-        <button
-          key={i}
-          className={`px-4 py-2 mx-1 rounded-md ${
-            i === currentPage
-              ? "bg-blue-500 text-white"
-              : "bg-gray-200 text-gray-900"
-          }`}
-          onClick={() => handlePageChange(i)}
-        >
-          {i}
-        </button>
-      );
+    if (totalPages <= 5) {
+      // Render all page buttons if total pages are less than or equal to 5
+      for (let i = 1; i <= totalPages; i++) {
+        paginationItems.push(
+          <button
+            key={i}
+            className={`px-4 py-2 mx-1 rounded-md ${
+              i === currentPage
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-900"
+            }`}
+            onClick={() => handlePageChange(i)}
+          >
+            {i}
+          </button>
+        );
+      }
+    } else {
+      // Render 5 page buttons and navigation arrows for more than 5 total pages
+      const startPage = Math.max(1, currentPage - 2);
+      const endPage = Math.min(startPage + 4, totalPages);
+
+      if (startPage > 1) {
+        paginationItems.push(
+          <button
+            key="previous"
+            className="px-4 py-2 mx-1 rounded-md bg-gray-200 text-gray-900"
+            onClick={handlePreviousPage}
+          >
+            &lt;
+          </button>
+        );
+      }
+
+      for (let i = startPage; i <= endPage; i++) {
+        paginationItems.push(
+          <button
+            key={i}
+            className={`px-4 py-2 mx-1 rounded-md ${
+              i === currentPage
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-900"
+            }`}
+            onClick={() => handlePageChange(i)}
+          >
+            {i}
+          </button>
+        );
+      }
+
+      if (endPage < totalPages) {
+        paginationItems.push(
+          <button
+            key="next"
+            className="px-4 py-2 mx-1 rounded-md bg-gray-200 text-gray-900"
+            onClick={handleNextPage}
+          >
+            &gt;
+          </button>
+        );
+      }
     }
 
     return paginationItems;
--- src/app/CoinApi/CriptoList1.js
+++ src/app/CoinApi/CriptoList1.js
@@ -2,13 +2,41 @@ import React from "react";
 import Image from "next/image";
 
 const CryptoList1 = ({ cryptocurrencies, exchangeRate }) => {
+  const formatPercentage = (percentage) => {
+    const roundedPercentage = parseFloat(percentage).toFixed(3);
+    return roundedPercentage;
+  };
+
+  const getPercentageColor = (percentage) => {
+    if (percentage < 0) {
+      return "text-red-500";
+    } else if (percentage > 0) {
+      return "text-green-500";
+    } else {
+      return "text-gray-500";
+    }
+  };
+
+  const formatCurrency = (value, currency) => {
+    return `${currency}${value.toLocaleString()}`;
+  };
+
   return (
     <ul className="mt-4 space-y-4 bg-gray-100 rounded-md divide-y divide-gray-200">
-      <div className="bg-gray-100 flex justify-end">
+      <div className="bg-green-500 text-white flex justify-end mr-4-">
         <div className="w-full md:w-5/6 flex flex-wrap">
-          <div className="w-1/3">Current Price (USD)</div>
-          <div className="w-1/3">Current Price (UAH)</div>
-          <div className="w-1/3">24-hour Price Change</div>
+          <div className="w-1/4 font-semibold hidden md:block">
+            Current Price (USD)
+          </div>
+          <div className="w-1/4 font-semibold hidden md:block">
+            Current Price (UAH)
+          </div>
+          <div className="w-1/4 font-semibold hidden md:block">
+            1-hour Price Change
+          </div>
+          <div className="w-1/4 font-semibold hidden md:block">
+            24-hour Price Change
+          </div>
         </div>
       </div>
       {cryptocurrencies.map((crypto) => (
@@ -25,17 +53,40 @@ const CryptoList1 = ({ cryptocurrencies, exchangeRate }) => {
                 height={35}
               />
             </div>
-            <h2 className="text-lg font-semibold">{crypto.name}</h2>
+            <h2 className="text-lg font-semibold mr-4">{crypto.name}</h2>
           </div>
           <div className="flex flex-col w-full md:w-5/6 md:flex-row md:flex-wrap">
-            <div className="text-gray-500 w-full md:w-1/3 md:flex md:items-center mb-4 md:mb-0">
-              <div>{crypto.current_price}</div>
+            <div className="text-gray-500 w-full md:w-1/4 md:flex md:items-center mb-4 md:mb-0">
+              <div className="font-medium">${crypto.current_price}</div>
             </div>
-            <div className="text-gray-500 w-full md:w-1/3 md:flex md:items-center mb-4 md:mb-0">
-              <div>{crypto.current_price * exchangeRate}</div>
+            <div className="text-gray-500 w-full md:w-1/4 md:flex md:items-center mb-4 md:mb-0">
+              <div className="font-medium">
+                {formatCurrency(crypto.current_price * exchangeRate, "₴")}
+              </div>
             </div>
-            <div className="text-gray-500 w-full md:w-1/3 md:flex md:items-center mb-4 md:mb-0">
-              <div>{crypto.price_change_percentage_24h_in_currency}%</div>
+            <div
+              className={`w-full md:w-1/4 md:flex md:items-center mb-4 md:mb-0 ${getPercentageColor(
+                crypto.price_change_percentage_1h_in_currency
+              )}`}
+            >
+              <div className="font-medium">
+                {formatPercentage(
+                  crypto.price_change_percentage_1h_in_currency
+                )}
+                %
+              </div>
+            </div>
+            <div
+              className={`w-full md:w-1/4 md:flex md:items-center mb-4 md:mb-0 ${getPercentageColor(
+                crypto.price_change_percentage_24h_in_currency
+              )}`}
+            >
+              <div className="font-medium">
+                {formatPercentage(
+                  crypto.price_change_percentage_24h_in_currency
+                )}
+                %
+              </div>
             </div>
           </div>
         </li>
